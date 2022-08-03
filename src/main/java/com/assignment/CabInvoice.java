4d9b61c488c21a25c3c8f8fc0fd8abf4e010a05b
Jeswin2711 <jeswin2711@gmail.com>
2022-08-03 19:29:53 +0530

    Added : Code for Calculating Fare for Multiple Rides

--- src/main/java/com/assignment/CabInvoice.java
+++ src/main/java/com/assignment/CabInvoice.java
@@ -11,4 +11,17 @@ public class CabInvoice
         if((distance * 10) + time < 5 ) return 5;
         else return totalFare;
     }
+
+    /*
+    Function to Calculate the Total Fare for Multiple Rides
+     */
+    public double calculateTotalFare(double[][] rideArray)
+    {
+        double totalFare = 0;
+        for(double[] ride : rideArray)
+        {
+            totalFare += calculateFare(ride[0],ride[1]);
+        }
+        return totalFare;
+    }
 }
